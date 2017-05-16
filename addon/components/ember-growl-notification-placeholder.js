8654029874eeaafc0f484f45b7c8ab949ca1e209
ykaragol <ykaragol@gmail.com>
2017-05-17 00:38:13 +0300

    added ember-growl-notification-placeholder as component

--- addon/components/ember-growl-notification-placeholder.js
+++ addon/components/ember-growl-notification-placeholder.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
+import layout from '../templates/components/ember-growl-notification-placeholder';
 
 export default Ember.Component.extend({
+  layout,
   service : Ember.inject.service('ember-growl-service'),
 
   notifications: Ember.computed.alias('service.notifications'),
