aeff280b654d0bdbcd0a571cbc10fef3f6bd509f
ykaragol <ykaragol@gmail.com>
2017-05-21 23:42:57 +0300

    page leave animation added, (now components heavily rely on css)

--- addon/components/ember-growl-notification-item-box.js
+++ addon/components/ember-growl-notification-item-box.js
@@ -1,18 +1,27 @@
 import Ember from 'ember';
 import layout from '../templates/components/ember-growl-notification-item-box';
 
-function animationendHandler(event){
-  if(event.animationName==='getsmaller'){
-    this.get('close')();
-  }
-}
+
 
 export default Ember.Component.extend({
   layout,
+  classNames:['ember-growl-notification-item-container'],
+  classNameBindings:['readyToClose:ready-to-close'],
+  readyToClose:false,
 
   didRender(){
     let animator = this.element.querySelector(".ember-growl-notification-item-box-time-to-die");
-    animator.addEventListener("animationend", animationendHandler.bind(this), false);
+    animator.addEventListener("animationend", (event)=>{
+      if(event.animationName==='getsmaller'){
+        this.set('readyToClose', true);
+      }
+    }, false);
+
+    this.element.addEventListener("animationend", (event)=>{
+      if(event.animationName==='readytoclose'){
+        this.get('close')();
+      }
+    }, false);
   },
 
   actions:{
