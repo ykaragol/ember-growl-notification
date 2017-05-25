14641157470d5c59149c03737826178af294b410
yusuf.karagol <yusuf.karagol@intra.local>
2017-05-25 13:55:44 +0300

    remove content-for from index.js, has problems with ember-twiddle

--- index.js
+++ index.js
@@ -7,11 +7,5 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
     app.import('vendor/ember-growl-notification.css');
-  },
-
-  contentFor(type, config) {
-    if (type === 'body-footer') {
-      return '<div id="growl-container" class="ember-growl-container"></div>';
-    }
   }
 };
