c80f653aa1a6a6e49b0ed161fc3cf073f6c5ef4e
ykaragol <ykaragol@gmail.com>
2017-05-26 01:06:06 +0300

    message types: success,error,info are added

--- addon/components/ember-growl-notification-item.js
+++ addon/components/ember-growl-notification-item.js
@@ -4,8 +4,13 @@ import layout from '../templates/components/ember-growl-notification-item';
 export default Ember.Component.extend({
   layout,
   classNames:['ember-growl-notification-item-container'],
-  classNameBindings:['readyToClose:ready-to-close'],
+  classNameBindings:['readyToClose:ready-to-close','typeClass'],
   readyToClose:false,
+
+  typeClass:Ember.computed('type',function(){
+    let type = this.get('type') || 'success';
+    return 'ember-growl-notification-'+type;
+  }),
 
   didRender(){
     let animator = this.element.querySelector(".ember-growl-notification-item-time-to-die");
